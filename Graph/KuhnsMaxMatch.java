46106ad2da85b4fedb3e2cc85a480c11dc541ad6
priyanshukmr <87386297+priyanshukmr@users.noreply.github.com>
2024-10-16 22:52:22 +0530

    Update KuhnsMaxMatch.java

--- Graph/KuhnsMaxMatch.java
+++ Graph/KuhnsMaxMatch.java
@@ -10,6 +10,81 @@ import java.util.*;
 
 class Main {
 
+    static class Graph {
+        int n;
+        ArrayList<Integer>[] adj;
+        ArrayList<Integer> left;
+        ArrayList<Integer> right;
+        
+        Graph(int n) {
+            this.n = n;
+            adj = new ArrayList[n+1];
+            left = new ArrayList<>();
+            for(int i=0; i<=n; i++) {
+                adj[i] = new ArrayList<>();
+            }
+        }
+        
+        void addEdge(int u, int v) {
+            adj[u].add(v);
+            adj[v].add(u);
+        }
+        
+        boolean tryAugmentingPath(int u, boolean[] vis, int[] match) {
+            if(vis[u]) return false;
+            vis[u] = true;
+            
+            for(int v: adj[u]) {
+                // either v is unsaturated or match[v] found a augmented path
+                if(match[v]==0 || tryAugmentingPath(match[v], vis, match)) {
+                    match[v] = u;
+                    return true;
+                }
+            }
+            return false;
+        }
+        
+        int kuhnMaxMatching() {
+            int matchCount = 0;
+            boolean[] vis = new boolean[n+1];
+            int[] match = new int[n+1];
+            
+            for(int u: left) {
+                Arrays.fill(vis, false);
+                if(tryAugmentingPath(u, vis, match)) {
+                    matchCount++;
+                }
+            }
+            return matchCount;
+        }
+        
+        void splitDfs(int u, int parent, int level) {
+            if(level%2==0) {
+                left.add(u);
+            } 
+            for(int v: adj[u]) {
+                if(v!=parent) {
+                    splitDfs(v, u, level+1);
+                }
+            }
+        }
+    }
+    
+    public static void main(String args[]) throws IOException {
+        // BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        Reader r = new Reader();
+        int n = r.nextInt();
+        Graph graph = new Graph(n);
+        
+        for(int i=0; i<n-1; i++) {
+            int u = r.nextInt();
+            int v = r.nextInt();
+            graph.addEdge(u, v);
+        }
+        graph.splitDfs(1, -1, 0);
+        System.out.println(graph.kuhnMaxMatching());
+    }
+
     static class Reader { 
         final private int BUFFER_SIZE = 1 << 16; 
         private DataInputStream din; 
@@ -132,80 +207,4 @@ class Main {
             din.close(); 
         } 
     }
-
-    static class Graph {
-        int n;
-        ArrayList<Integer>[] adj;
-        ArrayList<Integer> left;
-        ArrayList<Integer> right;
-        
-        Graph(int n) {
-            this.n = n;
-            adj = new ArrayList[n+1];
-            left = new ArrayList<>();
-            for(int i=0; i<=n; i++) {
-                adj[i] = new ArrayList<>();
-            }
-        }
-        
-        void addEdge(int u, int v) {
-            adj[u].add(v);
-            adj[v].add(u);
-        }
-        
-        boolean tryAugmentingPath(int u, boolean[] vis, int[] match) {
-            if(vis[u]) return false;
-            vis[u] = true;
-            
-            for(int v: adj[u]) {
-                // either v is unsaturated or match[v] found a augmented path
-                if(match[v]==0 || tryAugmentingPath(match[v], vis, match)) {
-                    match[v] = u;
-                    return true;
-                }
-            }
-            return false;
-        }
-        
-        int kuhnMaxMatching() {
-            int matchCount = 0;
-            boolean[] vis = new boolean[n+1];
-            int[] match = new int[n+1];
-            
-            for(int u: left) {
-                Arrays.fill(vis, false);
-                if(tryAugmentingPath(u, vis, match)) {
-                    matchCount++;
-                }
-            }
-            return matchCount;
-        }
-        
-        void splitDfs(int u, int parent, int level) {
-            if(level%2==0) {
-                left.add(u);
-            } 
-            for(int v: adj[u]) {
-                if(v!=parent) {
-                    splitDfs(v, u, level+1);
-                }
-            }
-        }
-    }
-    
-    public static void main(String args[]) throws IOException {
-        // BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        Reader r = new Reader();
-        int n = r.nextInt();
-        Graph graph = new Graph(n);
-        
-        for(int i=0; i<n-1; i++) {
-            int u = r.nextInt();
-            int v = r.nextInt();
-            graph.addEdge(u, v);
-        }
-        graph.splitDfs(1, -1, 0);
-        System.out.println(graph.kuhnMaxMatching());
-    }
-
 }
