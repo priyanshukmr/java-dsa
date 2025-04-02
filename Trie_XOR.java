869f7a48cd6432ecc46594291bc4ac28eeec6c9a
priyanshukmr <87386297+priyanshukmr@users.noreply.github.com>
2025-04-03 00:33:26 +0530

    Update Trie_XOR.java

--- Trie_XOR.java
+++ Trie_XOR.java
@@ -39,14 +39,35 @@ class Solution {
             }
             return maxor;
         }
+
+        // optimised 
+        int insertAndSearch(int n) {
+            Node insertPtr = root;
+            Node queryPtr = root;
+            int maxor = 0;
+            for(int i=31; i>=0; i--) {
+                int bit = ((1<<i)&n) > 0 ? 1: 0;
+                if(insertPtr.children[bit]==null) {
+                    insertPtr.children[bit] = new Node();
+                }
+                insertPtr= insertPtr.children[bit];
+                if(queryPtr.children[bit^1]!=null) {
+                    maxor |= (1<<i);
+                    queryPtr = queryPtr.children[bit^1];
+                }
+                else {
+                    queryPtr = queryPtr.children[bit];
+                }
+            }
+            return maxor;
+        }
     }
     
     public int findMaximumXOR(int[] nums) {
         Trie trie = new Trie();
         int ans=0;
         for(int num: nums) {
-            trie.insert(num);
-            ans = Math.max(ans, trie.searchMax(num));
+            ans = Math.max(ans, trie.insertAndSearch(num));
         }
         return ans;
     }
