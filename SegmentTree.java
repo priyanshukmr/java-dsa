63baeee33452c630eafeccd7f8be2f7073565ce1
priyanshukmr <87386297+priyanshukmr@users.noreply.github.com>
2025-10-24 00:48:20 +0530

    Update SegmentTree.java

--- SegmentTree.java
+++ SegmentTree.java
@@ -1,70 +1,200 @@
-// point update, range min
+// point update, range min (https://cses.fi/problemset/task/1649/)
 // Segment tree also works for non-invertible functions like min,max while fenwick does not
 
 import java.io.*;
-import java.util.*;
- 
- 
-class SegmentTree {
-    
-    static int SZ = 100000;
-    static int st[] = new int[8*SZ+10];
-    
-    static void update(int i, int pos, int ss, int se, int val) {                   
-        if(ss>pos || se<pos) {return;}
-        if(ss==se) {
-            st[i]=val;
-            return;
+
+class Main {
+
+    static class SegmentTree {
+        private int n;
+        private int tree[];
+
+        SegmentTree(int n) {
+            this.n = n;
+            tree = new int[4*n+10];
         }
-        int mid = (ss+se)>>1;
-        update(2*i, pos, ss, mid, val);
-        update(2*i+1, pos, mid+1, se, val);
-        st[i] = Math.min(st[2*i], st[2*i+1]);
+        private void updateUtil(int i, int ss, int se, int pos, int val) {
+            if(ss>pos || se<pos) {return;}
+            if(ss==se) {
+                tree[i]=val;
+                return;
+            }
+            int mid = (ss+se)>>1;
+            updateUtil(2*i+1, ss, mid, pos, val);
+            updateUtil(2*i+2, mid+1, se, pos, val);
+            tree[i] = Math.min(tree[2*i+1], tree[2*i+2]);
+        }
+
+        private int queryUtil(int i, int ss, int se, int l, int r) {
+            if(se<l || ss>r) return Integer.MAX_VALUE;
+            if(ss>=l && se<=r) {
+                return tree[i];
+            }
+            int mid = (ss+se)/2;
+            int leftMin = queryUtil(2*i+1, ss, mid, l, r);
+            int rightMin = queryUtil(2*i+2, mid+1, se, l, r);
+            return Math.min(leftMin, rightMin);
+        }
+
+        public void update(int pos, int val) {
+            updateUtil(0, 0, n-1, pos, val);
+        }
+
+        public  int query(int l, int r) {
+            return queryUtil(0, 0, n-1, l, r);
+        }
     }
-    
-    static int query(int i, int ss, int se, int l, int r) {
-        if(se<l || ss>r) return Integer.MAX_VALUE;
-        if(ss>=l && se<=r) {
-            return st[i];
-        } 
-        int mid = (ss+se)/2;
-        int leftMin = query(2*i, ss, mid, l, r);
-        int rightMin = query(2*i+1, mid+1, se, l, r);
-        return Math.min(leftMin, rightMin);
-    }
-    
-    
- 
+
     public static void main(String args[]) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        Reader rd = new Reader();
         StringBuilder out = new StringBuilder();
-        
-        String inp[] = br.readLine().split(" ");
-        int n = Integer.parseInt(inp[0]);
-        int q = Integer.parseInt(inp[1]);
-        inp = br.readLine().split(" ");
-        
+
+        int n = rd.nextInt();
+        int q = rd.nextInt();
+        SegmentTree segmentTree  = new SegmentTree(n);
         for(int i=0; i<n; i++) {
-            int val = Integer.parseInt(inp[i]);
-            update(1, i, 0, n-1, val);
+            segmentTree.update(i, rd.nextInt());
         }
-        
+
         while(q-->0) {
-            inp = br.readLine().split(" ");
+            int type = rd.nextInt();
             //update
-            if(inp[0].equals("1")) {
-                int k = Integer.parseInt(inp[1]);
-                int u = Integer.parseInt(inp[2]);
-                update(1, k-1, 0, n-1, u);
+            if(type==1) {
+                int k = rd.nextInt();
+                int u = rd.nextInt();
+                segmentTree.update(k-1, u);
             }
             //query
             else {
-                int a = Integer.parseInt(inp[1]);
-                int b = Integer.parseInt(inp[2]);
-                int minn = query(1, 0, n-1, a-1, b-1);
+                int a = rd.nextInt();
+                int b = rd.nextInt();
+                int minn = segmentTree.query(a-1, b-1);
                 out.append(String.valueOf(minn) + "\n");
             }
         }
         System.out.println(out);
+    }
+
+    static class Reader {
+        final private int BUFFER_SIZE = 1 << 16;
+        private DataInputStream din;
+        private byte[] buffer;
+        private int bufferPointer, bytesRead;
+
+        public Reader()
+        {
+            din = new DataInputStream(System.in);
+            buffer = new byte[BUFFER_SIZE];
+            bufferPointer = bytesRead = 0;
+        }
+
+        public Reader(String file_name) throws IOException
+        {
+            din = new DataInputStream(
+                    new FileInputStream(file_name));
+            buffer = new byte[BUFFER_SIZE];
+            bufferPointer = bytesRead = 0;
+        }
+
+        public String readLine() throws IOException
+        {
+            byte[] buf = new byte[3005]; // line length
+            int cnt = 0, c;
+            while ((c = read()) != -1) {
+                if (c == '\n') {
+                    if (cnt != 0) {
+                        break;
+                    }
+                    else {
+                        continue;
+                    }
+                }
+                buf[cnt++] = (byte)c;
+            }
+            return new String(buf, 0, cnt);
+        }
+
+        public int nextInt() throws IOException
+        {
+            int ret = 0;
+            byte c = read();
+            while (c <= ' ') {
+                c = read();
+            }
+            boolean neg = (c == '-');
+            if (neg)
+                c = read();
+            do {
+                ret = ret * 10 + c - '0';
+            } while ((c = read()) >= '0' && c <= '9');
+
+            if (neg)
+                return -ret;
+            return ret;
+        }
+
+        public long nextLong() throws IOException
+        {
+            long ret = 0;
+            byte c = read();
+            while (c <= ' ')
+                c = read();
+            boolean neg = (c == '-');
+            if (neg)
+                c = read();
+            do {
+                ret = ret * 10 + c - '0';
+            } while ((c = read()) >= '0' && c <= '9');
+            if (neg)
+                return -ret;
+            return ret;
+        }
+
+        public double nextDouble() throws IOException
+        {
+            double ret = 0, div = 1;
+            byte c = read();
+            while (c <= ' ')
+                c = read();
+            boolean neg = (c == '-');
+            if (neg)
+                c = read();
+
+            do {
+                ret = ret * 10 + c - '0';
+            } while ((c = read()) >= '0' && c <= '9');
+
+            if (c == '.') {
+                while ((c = read()) >= '0' && c <= '9') {
+                    ret += (c - '0') / (div *= 10);
+                }
+            }
+
+            if (neg)
+                return -ret;
+            return ret;
+        }
+
+        private void fillBuffer() throws IOException
+        {
+            bytesRead = din.read(buffer, bufferPointer = 0,
+                    BUFFER_SIZE);
+            if (bytesRead == -1)
+                buffer[0] = -1;
+        }
+
+        private byte read() throws IOException
+        {
+            if (bufferPointer == bytesRead)
+                fillBuffer();
+            return buffer[bufferPointer++];
+        }
+
+        public void close() throws IOException
+        {
+            if (din == null)
+                return;
+            din.close();
+        }
     }
 }
